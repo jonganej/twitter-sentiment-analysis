e1db01d2d49874cf10db98cf9dad05a7767b60db
Jonathan Gan <jongan@student.ethz.ch>
2017-05-10 12:31:50 +0200

    Made necessary amendments such that no nonalgorithmic errors occur

--- dataloader/tweetsloader.py
+++ dataloader/tweetsloader.py
@@ -1,5 +1,6 @@
 import collections, csv, math, random
 import numpy as np
+import tensorflow as tf
 
 class TweetsDataLoader:
     def __init__(self, filename_csv, batch_size, fraction_training=0.8, seed=42):
@@ -16,7 +17,7 @@ class TweetsDataLoader:
         count_pairs = sorted(counter.items(), key=lambda x: -x[1])
         chars, _ = zip(*count_pairs)
         vocab = dict(zip(chars, range(len(chars))))
-
+        self.vocab = vocab
         # shuffle both content and labels
         random.seed(seed)
         random.shuffle(content)
@@ -25,19 +26,25 @@ class TweetsDataLoader:
 
         # encoded is list of list of chars
         self.encoded = [[vocab[k] for k in tweet] for tweet in content]
+        self.original_seq_length = [len(t) for t in self.encoded]
         self.encoded = self._pad_data(self.encoded)
-        self.training_text = self.encoded[0:int(fraction_training * len(self.encoded))]
-        self.validation_text = self.encoded[int(fraction_training * len(self.encoded)):]
-        self.training_label = labels[0:int(fraction_training * len(self.encoded))]
-        self.validation_label = labels[int(fraction_training * len(self.encoded)):]
+        self.training_text, self.validation_text = self._divide_data(self.encoded, fraction_training)
+        self.training_label, self.validation_label = self._divide_data(labels, fraction_training)
+        self.training_seq_lengths, self.validation_seq_lengths = self._divide_data(self.original_seq_length, fraction_training)
         self.num_batches = math.ceil(len(self.training_label) / batch_size)
-        self.training_batches_text = [self.training_text[i:i+batch_size]
-                                      for i in range(0, len(self.training_text), batch_size)]
+        self.training_batches_text = self._create_batches(self.training_text, batch_size)
 
-        self.training_batches_labels = [self.training_label[i:i+batch_size]
-                                        for i in range(0, len(self.training_label), batch_size)]
+        self.training_batches_labels = self._create_batches(self.training_label, batch_size)
 
+        self.training_batches_seqlength = self._create_batches(self.training_seq_lengths, batch_size)
         self.pointer = 0
+
+    def _divide_data(self, data, frac_training):
+        divide_idx = int(frac_training * len(data))
+        return data[0:divide_idx], data[divide_idx:]
+
+    def _create_batches(self, data, batch_size):
+        return [data[i:i+batch_size] for i in range(0, len(data), batch_size)]
 
     def _pad_data(self, data):
         # get max length
@@ -49,17 +56,20 @@ class TweetsDataLoader:
 
     def get_next_training_batch(self):
         x, y = self.training_batches_text[int(self.pointer % self.num_batches)], self.training_batches_labels[int(self.pointer % self.num_batches)]
+        seq_lengths = self.training_batches_seqlength[int(self.pointer % self.num_batches)]
         self.pointer += 1
-        return np.array(x), np.array(y)
+        return np.array(x), np.array(y), np.array(seq_lengths)
 
     def get_validation_data(self):
         return np.array(self.validation_text), np.array(self.validation_label)
 
+    def reset_pointer(self):
+        self.pointer = 0
 
 if __name__ == '__main__':
-    tweet_data = '/Users/jongan/repositories/twitter-sentiment-analysis/data/oldBrexitSentimentComparison.csv'
+    tweet_data = '/home/ganenjij/repositories/twitter-sentiment-analysis/data/oldBrexitSentimentComparison.csv'
     tweetsloader = TweetsDataLoader(tweet_data, 32)
-    x, y = tweetsloader.get_next_training_batch()
+    x, y, seq_length = tweetsloader.get_next_training_batch()
     valid_x, valid_y = tweetsloader.get_validation_data()
     print (len(x))
     print (len(valid_x))
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -5,7 +5,8 @@ import tensorflow as tf
 from tensorflow.contrib import rnn
 from tensorflow import nn
 import biwigpuprep
-import os, time
+from dataloader import tweetsloader
+import os, sys, time, yaml
 
 
 def lazy_property(function):
@@ -22,61 +23,85 @@ def lazy_property(function):
 
 class VariableSequenceClassification:
 
-    def __init__(self, data, target, num_hidden=200, num_layers=2):
-        self.data = data
-        self.target = target
-        self._num_hidden = num_hidden
-        self._num_layers = num_layers
+    def __init__(self, vocab_size, params):
+        self.data = tf.placeholder(tf.int32, [params['batch_size'], n_steps])
+        self.target = tf.placeholder(tf.int32, [params['batch_size'], n_classes])
+        self.sequence_lengths = tf.placeholder(tf.int32, [params['batch_size']])
+        self.vocab_size = vocab_size
+
+        if 'n_layers' in params:
+            self._num_layers = params['n_layers']
+        else:
+            self._num_layers = 2
+
+        if 'n_hidden' in params:
+            if isinstance(params['n_hidden'], list):
+                self._num_hidden = params['n_hidden']
+            else:
+                self._num_hidden = [params['n_hidden']] * self._num_layers
+        else:
+            self._num_hidden = [128, 128]
+
+        self.params = params
+
+        # build graph
         self.prediction
         self.error
         self.optimize
 
     @lazy_property
-    def length(self):
-        used = tf.sign(tf.reduce_max(tf.abs(self.data), reduction_indices=2))
-        length = tf.reduce_sum(used, reduction_indices=1)
-        length = tf.cast(length, tf.int32)
-        return length
-
-    @lazy_property
     def prediction(self):
         # Recurrent network.
         cells = []
+
+        # dropout
+        if 'dropout' in self.params:
+            keep_prob = tf.constant(self.params['dropout'])
+        else:
+            keep_prob = tf.constant(0.6)
+
         # create multi layer RNN
-        for _ in range(self._num_layers):
-            cell = rnn.GRUCell(self._num_hidden)
+        for i in range(self._num_layers):
+            cell = rnn.GRUCell(self._num_hidden[i])
+            cell = rnn.DropoutWrapper(cell, input_keep_prob=keep_prob, output_keep_prob=keep_prob)
             cells.append(cell)
 
-        init_state = tf.get_variable('init_state', [1, self._num_hidden],
-                                     initializer=tf.constant_initializer(0.0))
-        init_state = tf.tile(init_state, [batch_size, 1])
+        # embedding layer
+        embeddings = tf.get_variable('embedding_matrix', [self.vocab_size, self._num_hidden[-1]])
+        rnn_inputs = tf.nn.embedding_lookup(embeddings, self.data)
+
+        # init_state = tf.get_variable('init_state', [1, self._num_hidden[-1]],
+        #                              initializer=tf.constant_initializer(0.0))
+        # init_state = tf.tile(init_state, [batch_size, 1])
         output, _ = nn.dynamic_rnn(
             rnn.MultiRNNCell(cells, state_is_tuple=True),
-            data,
+            rnn_inputs,
             dtype=tf.float32,
             # initial_state=init_state,
-            sequence_length=self.length,
+            sequence_length=self.sequence_lengths,
         )
         # last = tf.gather_nd(output, tf.stack([tf.range(128), self.length-1], axis=1))
-        keep_prob = tf.constant(0.6)
-        output = tf.nn.dropout(output, keep_prob)
-
-        last = self._last_relevant(output, self.length)
+        last = self._last_relevant(output, self.sequence_lengths)
         # Softmax layer.
         weight, bias = self._weight_and_bias(
-            self._num_hidden, int(self.target.get_shape()[1]))
+            self._num_hidden[-1], int(self.target.get_shape()[1]))
         prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
         return prediction
 
     @lazy_property
     def cost(self):
         with tf.name_scope("cost"):
-            cross_entropy = -tf.reduce_sum(self.target * tf.log(self.prediction))
+            target_f = tf.cast(self.target, tf.float32)
+            pred_f = tf.cast(self.prediction, tf.float32)
+            cross_entropy = -tf.reduce_sum(target_f * tf.log(pred_f))
         return cross_entropy #tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.target, logits=self.prediction))
 
     @lazy_property
     def optimize(self):
-        learning_rate = 0.001
+        if 'learning_rate' in self.params:
+            learning_rate = self.params['learning_rate']
+        else:
+            learning_rate = 0.001
         with tf.name_scope("optimizer"):
             optimizer = tf.train.AdamOptimizer(learning_rate)
         return optimizer.minimize(self.cost)
@@ -94,7 +119,6 @@ class VariableSequenceClassification:
             bias = tf.get_variable("softmax_b", shape=(out_size), initializer=tf.zeros_initializer)
         return weight, bias
 
-
     @staticmethod
     def _last_relevant(output, length):
         batch_size = tf.shape(output)[0]
@@ -108,17 +132,26 @@ class VariableSequenceClassification:
 
 if __name__ == '__main__':
     # hyper params
-    batch_size = 128
-    n_epoch = 500
-    initialize_from_saved = True
-    save_dir = './save'
-    save_every = 100
-    data_loader = mhadloader.MHADDataLoader('/home/ganenjij/my_scratch/UTD-MHAD-dataset-padded/', batch_size)
-    n_steps, n_inputs, n_classes = 125, 60, 27
+    config_filename = sys.argv[1]
+    with open(config_filename, 'r') as f:
+        hyperparams = yaml.load(f)
+        print ("Loaded config file from %s" % config_filename)
 
-    data = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
-    target = tf.placeholder(tf.float32, [None, n_classes])
-    model = VariableSequenceClassification(data, target, 128, 2)
+    batch_size = hyperparams['batch_size']
+    n_epoch = hyperparams['n_epoch']
+    initialize_from_saved = hyperparams['initialize_from_saved']
+    save_dir = hyperparams['save_dir']
+
+    # create save dir if doesn't yet exist
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+
+    save_every = hyperparams['save_every']
+
+    data_loader = tweetsloader.TweetsDataLoader('/home/ganenjij/repositories/twitter-sentiment-analysis/data/oldBrexitSentimentComparison.csv', batch_size)
+    n_steps, n_inputs, n_classes = data_loader.sequence_length, 1, 2
+
+    model = VariableSequenceClassification(len(data_loader.vocab), hyperparams)
 
     # validation data
     valid_data, valid_target = data_loader.get_validation_data()
@@ -140,9 +173,9 @@ if __name__ == '__main__':
             start_epoch = time.time()
             data_loader.reset_pointer()
             for i in range(data_loader.num_batches):
-                train_x, train_y = data_loader.get_next_training_batch()
-                sess.run(model.optimize, {data: train_x, target: train_y})
-                length = sess.run(model.length, {data: train_x})
+                train_x, train_y, train_seqlength = data_loader.get_next_training_batch()
+                sess.run(model.optimize, {model.data: train_x, model.target: train_y, model.sequence_lengths: train_seqlength})
+                # length = sess.run(model.length, {model.data: train_x})
                 if (epoch * data_loader.num_batches + i) % save_every == 0 or \
                         (epoch == n_epoch - 1 and i == data_loader.num_batches - 1):
                     iter = epoch * data_loader.num_batches + i
@@ -150,7 +183,7 @@ if __name__ == '__main__':
                     saver.save(sess, chkpt_path, global_step=iter)
                     print ("Iteration no. {}, Model saved to {}".format(iter, chkpt_path))
 
-            error = sess.run(model.error, {data: valid_data, target: valid_target})
+            error = sess.run(model.error, {model.data: valid_data, model.target: valid_target})
             print('Epoch {:2d} error {:3.1f}% -- time taken: {:3.3f}'.format(epoch + 1, 100 * error, time.time() - start_epoch))
 
     print("Total time taken: %s" % (time.time() - start_global))
